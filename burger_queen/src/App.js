baab977247c1374e18bcd6d89a04d88e642a426b
Eira Mazon <eira.b.mazon@gmail.com>
2021-05-03 16:22:35 -0500

    feat(json-server): fetch and database working, menu and taking orders enabled

--- burger_queen/src/App.js
+++ burger_queen/src/App.js
@@ -1,6 +1,7 @@
 import { Login } from './Components/Login/Login.js';
 import { OrderMenu } from './Components/OrderMenu/OrdersMenu.js';
 import { TakeOrder } from './Components/TakeOrder/TakeOrder.js';
+import { Orders } from './Components/Orders/Orders.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -14,8 +15,11 @@ function App() {
           <Route exact path="/">
             <OrderMenu />
           </Route>
-          <Route exact path="/orders">
+          <Route exact path="/neworder">
             <TakeOrder />
+          </Route>
+          <Route exact path="/orders">
+            <Orders />
           </Route>
         </Switch>
       </div>
--- burger_queen/src/Components/TakeOrder/TakeOrder.js
+++ burger_queen/src/Components/TakeOrder/TakeOrder.js
@@ -1,43 +1,58 @@
 import '../TakeOrder/TakeOrder.css';
 import React, { useState } from 'react';
 import Navbar from '../Elements/Navbar/Navbar.js';
-
+import { useEffect } from 'react';
 
 export const TakeOrder=()=> {
 
-	const [MenuItems, setMenuItems] = useState([
-		{item : "Café con leche", price : "", id : 1},
-		{item : "Café americano", price : "", id : 2},
-		{item : "Jugo natural", price : "", id : 3},
-		{item : "Sandwich jamón/queso", price : "", id : 4},
-	])
+	const [menu, setMenu] = useState(null)
+	const [orderItems, setOrderItems] = useState([]);
+	const [clientName, setClientName] = useState('')
 
-	const changeMenu = () =>{
-		setMenuItems([
-			{item : "Hamburguesa", price : "", id : 1},
-			{item : "Hamburguesa doble", price : "", id : 2},
-			{item : "Papas fritas", price : "", id : 3},
-			{item : "Aros de cebolla", price : "", id : 4},
-			{item : "Agua 500ml", price : "", id : 5},
-			{item : "Agua 750ml", price : "", id : 6},
-			{item : "Bebida/gaseosa 500ml", price : "", id : 7},
-			{item : "Bebida/gaseosa 750ml", price : "", id : 8},
 
-		])
-	}
+		useEffect(() => {	
+		
+		fetch('http://localhost:8000/items')
+		.then((res) =>res.json())
+		.then((data) => {setMenu(data)})
+		}, [])
+
+		const clicked = (item) => {
+			const exists = orderItems.find((x) => x.productId === item.productId);
+			if(exists){
+				setOrderItems(
+					orderItems.map((x) => x.productId === item.productId ? { ...exists, qty: exists.qty + 1 } : x
+					))
+			}else{
+				setOrderItems([...orderItems, { ...item, qty: 1 }]);
+			}
+		}
+
+		const sendOrder = () => {
+			const order = {
+				"client" : clientName,
+				"products" : orderItems
+			}
+
+			fetch('http://localhost:8000/orders', {
+				method: 'POST',
+				headers: { "Content-Type": "application/json"},
+				body: JSON.stringify(order)
+			}) 
+		}
 
 	return(
-		<div className="TakeOrder-Main" >
+		<div className="TakeOrder-Main">
 			<Navbar/>
 			<div className="TakeOrder-Container">
 				<div className="TakeOrder-Menu" >
-					<button onClick={changeMenu}>Desayuno</button>
-					<button onClick={changeMenu}>Almuerzo y cena</button>
+					<button>Desayuno</button>
+					<button>Almuerzo y cena</button>
 					<hr/>
 					<div className="items">
-					{MenuItems.map((item) => (
-						<div className="item-button" key={item.id}>
-							<h1>{item.item}</h1>
+					{menu && menu.map((item) => (
+						<div className="item-button" key={item.productId} onClick={()=>clicked(item)}>
+							<h1>{item.product}</h1>
 						</div>
 					))
 					}
@@ -45,19 +60,19 @@ export const TakeOrder=()=> {
 				</div>
 				<div className="Takeorder-Board-Container">
 					<div className="Takeorder-board">
-					<label>Orden</label>
-					<input className="user-name" type="text" placeholder="Nombre del cliente"></input>
-					<div className="mock-order">
-						<label> 1</label>
-						<label>Jugo de naranja:</label>
-						<label>$15.00</label><br/>
-						<label> 1</label>
-						<label>Torta de jamón</label>
-						<label>$25.00</label>
-
-					</div>
-						<label>Total:</label>
-					<button>Enviar a cocina</button>
+					<p>Orden</p>
+					<input className="user-name" type="text" required placeholder="Nombre del cliente"
+					value={clientName} onChange={(e) => setClientName(e.target.value)}></input>
+					<div className="order">
+						{orderItems.map((item) =>(
+							<p key={item.productId}>
+							{item.qty} {item.product} ${item.price * item.qty}
+							</p>
+						))
+						}
+					</div>					
+					<p>Total: ${orderItems.reduce((a,b) => a + b.price * b.qty, 0)}</p>
+					<button onClick={sendOrder}>Enviar a cocina</button>
 					</div>
 				</div>
 			</div>
