5a54d9101b2b3b578ab25b6e5fc415380a8e8541
osiris <wendyosi25@gmail.com>
2021-05-07 14:02:29 -0500

    inventario

--- burger_queen/src/Components/Elements/Inventario/inventario.js
+++ burger_queen/src/Components/Elements/Inventario/inventario.js
@@ -1,18 +1,27 @@
-
-import React from 'react';
+import './../../Elements/Inventario/inventario.css';
+import React, { useEffect, useState } from 'react';
 
 export default function Inventario() {
+const[products, setProducts]=useState(null);
+
+useEffect(()=>{
+	fetch('http://localhost:8000/inventary')
+	.then((res)=>res.json())
+	.then((data)=>{setProducts(data);})
+},[])
 
 	return (
-		<div className="container-registration">
-			<h1 id="title">Inventario de productos</h1>
-
-			<div className="card-inventario">
-				<p>nombre</p>
-				<image/>
-				<p>existencia</p>
-
-			</div>
+		<div className="container-products">
+				<h1 id="title">Inventario de productos</h1>
+				<div className="container-cards">
+				{products&&products.map((element)=>(
+					<div key={element.id}className="card-inventario">
+							<p className="name-product">Producto: {element.nameProduct}</p>
+							<img className="img-product" src={element.imgProduct} alt=""/>
+							<p className="existence-product">Existencia: {element.existenceProduct}</p>
+					</div>
+					))}
+					</div>
 		</div>
 	);
 }
